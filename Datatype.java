63fdfcaf5a08a5a4a38df8fe5b2ce0f9b8f0086e
vikas padhi <vikaspadhi5@gmail.com>
2021-11-06 17:45:30 +0530

    lect1

---
+++ Datatype.java
@@ -0,0 +1,62 @@
+public class Datatype {
+
+    public static void main (String []args)
+    {
+        /*String greeting = " Hello ";
+        String name = " aman ";
+        System.out.println (greeting + name);*/
+        
+        /*Float treat = 10f;
+        int net = 10;
+        System.out.println(treat + net);*/
+
+        //final int a= 20;
+        /*a=10 ;
+        System.out.println(a);*/
+
+        /*double b = 10.9;
+        int d=(int) b;
+        System.out.println(d);*/
+        /*int a= 10;
+        double b= a ;
+        System.out.println(b);*/
+        // int a= 10;
+        // a=a+1;
+        // a++ ;
+        // a=a-1;
+        // a-- ;
+        // System.out.println(a);
+        // int a= 20;
+        // int b= ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+        // int a=20;
+        // int b=a++;
+        // System.out.println(a);
+        // System.out.println(b);
+        // int a=10,b=11, c=9 ;
+        // int result;
+        // result = a++ + b++ + ++c;
+        // System.out.println (result);
+        // int a = 10;
+        // int b = 10;
+        // System.out.println(  !( a>5 || b<8 ) ); 
+        // System.out.println(  !( a>5 && b<8 ) ); 
+        // String A= "Hello";
+        // String B = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // String B = "Hello";
+        // System.out.println(" The length of the text string is "+B.length());
+        // System.out.println(A.charAt(3));
+        // System.out.println(A.toLowerCase());
+        // System.out.println(A.toUpperCase());
+        // System.out.println(A.indexOf("el"));
+        // System.out.println(A.concat(B));
+        // String s = "ell";
+        // System.out.println(A.contains(s));
+        // System.out.println(A.isEmpty());
+        // System.out.println(A.compareTo(B));
+        // String stm = "This is a \"statement";
+        // String stm = "This is a statement \n";
+        // System.out.print(stm);
+    }    
+}
